a3c02953b06317600ce8b6c78978294a6e71b13d
Ayush Gulhane <gulhanes2016@gmail.com>
2023-12-27 15:54:43 +0530

    BOM DEL no more

--- main.py
+++ main.py
@@ -5,27 +5,77 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from tkinter import ttk
 
-def on_date_select(cal, date_label):
-    global depVar
-    depVar = cal._sel_date
-    selected_date = cal._sel_date
-    date_label.config(text=f"Selected Date: {selected_date}")
+# def on_date_select(cal, date_label):
+#     global depVar
+#     depVar = cal._sel_date
+#     selected_date = cal._sel_date
+#     date_label.config(text=f"Selected Date: {selected_date}")
 
 
-def to_select():
-    global toVar
-    toVar = to_.get('1.0', 'end-1c')
-    print(toVar)
+# def to_select():
+#     global toVar
+#     toVar = to_.get('1.0', 'end-1c')
+#     print(toVar)
 
 
-def from_select():
-    global fromVar
-    fromVar = from_.get('1.0', 'end-1c')
-    print(fromVar)
+# def from_select():
+#     global fromVar
+#     fromVar = from_.get('1.0', 'end-1c')
+#     print(fromVar)
+
+###########################################################################################################################################################
+import sqlite3
 
 
-root = Tk()
-root.grid()
+con = sqlite3.connect('iata.db')
+cur = con.cursor()
+cur.execute("DELETE FROM Sheet1 WHERE [City name] = 'City name'")
+rows = cur.fetchall()
+print(rows)
+
+
+def checkkey(event,lb): 
+    value = event.widget.get() 
+    if value == '': 
+        data = l 
+    else: 
+        data = [item for item in l if item is not None and value.lower() in item.lower()] 
+    update(data,lb) 
+
+def update(data,lb): 
+    lb.delete(0, 'end') 
+    for item in data: 
+        lb.insert('end', item) 
+
+def on_listbox_click(event,lb,e):
+    selected_index = lb.curselection()
+    if selected_index:
+        selected_item = lb.get(selected_index)
+        e.delete(0, 'end')
+        e.insert(0, selected_item)
+
+r = cur.execute('SELECT * FROM Sheet1') 
+w = r.fetchall()
+l = ['|'.join(str(item) for item in t if item is not None) for t in w]
+con.commit()
+# root = Tk() 
+
+# e = Entry(root) 
+# e.pack() 
+# e.bind('<KeyRelease>', checkkey) 
+
+# lb = Listbox(root) 
+# lb.pack() 
+# update(l)
+
+# # Bind the listbox click event to the on_listbox_click function
+# lb.bind('<ButtonRelease-1>', on_listbox_click)
+
+# root.mainloop()
+
+###########################################################################################################################################################
+
+
 # root.grid_rowconfigure(0, weight=1)
 # root.grid_rowconfigure(1, weight=1)
 # root.grid_rowconfigure(2, weight=1)
@@ -38,9 +88,24 @@ root.grid()
 # root.grid_columnconfigure(3, weight=1)
 # root.grid_columnconfigure(4, weight=1)
 # root.grid_columnconfigure(5, weight=1)
-root.geometry('525x600')
+root = Tk()
+root.grid()
+root.geometry('525x800')
 root.resizable(width=False,height=False)
 frame = Frame(root)
+
+# e = Entry(root) 
+# e.pack() 
+# e.bind('<KeyRelease>', checkkey) 
+
+# lb = Listbox(root) 
+# lb.pack() 
+# update(l)
+
+# # Bind the listbox click event to the on_listbox_click function
+# lb.bind('<ButtonRelease-1>', on_listbox_click)
+
+
 
 frame.grid(row=0, column=0)
 toVar = StringVar()
@@ -50,22 +115,36 @@ arrVar = StringVar()
 
 fromLabel = Label(frame, text="From: ")
 fromLabel.grid(row=0, column=0,pady=10)
-from_ = Text(frame, height="1", width=50)
-from_.grid(row=0, column=1,pady=10)
+from_e = Entry(frame,width=50)
+from_e.grid(row=0, column=1)
+from_e.bind('<KeyRelease>', lambda event: checkkey(event,from_lb)) 
+from_lb = Listbox(frame,width=50,height=5) 
+from_lb.grid(row=1, column=1) 
+update(l,from_lb)
+from_lb.bind('<ButtonRelease-1>', lambda event: on_listbox_click(event,from_lb,from_e))
+# from_ = Text(frame, height="1", width=50)
+# from_.grid(row=0, column=1,pady=10)
 # fromButton = Button(frame, text='Select', command=from_select, bg='white')
 # fromButton.grid(row=0, column=2,pady=10)
 
 toLabel = Label(frame, text="To: ")
-toLabel.grid(row=1, column=0,pady=10)
-to_ = Text(frame, height="1", width=50)
-to_.grid(row=1, column=1,pady=10)
+toLabel.grid(row=2, column=0,pady=10)
+to_e = Entry(frame,width=50)
+to_e.grid(row=2, column=1)
+to_e.bind('<KeyRelease>', lambda event: checkkey(event,to_lb)) 
+to_lb = Listbox(frame,width=50,height=5) 
+to_lb.grid(row=3, column=1) 
+update(l,to_lb)
+to_lb.bind('<ButtonRelease-1>', lambda event: on_listbox_click(event,to_lb,to_e))
+# to_ = Text(frame, height="1", width=50)
+# to_.grid(row=1, column=1,pady=10)
 # toButton = Button(frame, text='Select', command=to_select, bg='white')
 # toButton.grid(row=1, column=2,pady=10)
 
 depLabel = Label(frame, text="Departure date: ")
-depLabel.grid(row=2, column=0,pady=10)
+depLabel.grid(row=4, column=0,pady=10)
 cal1 = Calendar(frame, selectmode="day")
-cal1.grid(row=2, column=1,pady=10)
+cal1.grid(row=4, column=1,pady=10)
 
 # select_button1 = Button(frame, text="Select Date", command=lambda: on_date_select(cal1, date_label), bg='white')
 # select_button1.grid(row=3, column=1,pady=10)
@@ -111,7 +190,9 @@ infant_select.grid(row=9,column=1,pady=10)
 # date_label2 = Label(frame, text="Selected Date: ")
 # date_label2.grid(row=4, column=2)
 # print(toVar.get())
-final_button = Button(frame, text="Fly!!", command=lambda: scrape_price( from_.get('1.0', 'end-1c'),to_.get('1.0', 'end-1c'), cal1._sel_date,adult_select.get(),child_select.get(),infant_select.get(),seat_select.get()), bg='coral',height=2,width=5)
+
+final_button = Button(frame, text="Fly!!", command=lambda: scrape_price( from_e.get().split('|')[1],to_e.get().split('|')[1],cal1._sel_date,adult_select.get(),child_select.get(),infant_select.get(),seat_select.get()), bg='coral',height=2,width=5)
 final_button.grid(row=10, column=1,pady=10)
 
+    
 root.mainloop()
--- scraping.py
+++ scraping.py
@@ -1,62 +1,66 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from display import display
+from international import scrape_price_i
 
 
 def scrape_price(dep, arr, travel_date,adult,child,infant,seat):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--window-size=1920,1080')  
-    chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(options=chrome_options)
-    # driver = webdriver.Chrome()
+    try:    
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--window-size=1920,1080')  
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options)
+        # driver = webdriver.Chrome()
 
-    source = dep.upper()
-    destination = arr.upper()
-    # passenger_count = "1"
-    adult_count = adult
-    child_count = child
-    infant_count = infant
-    seat_type = seat[0]
-    travel_date = travel_date
+        source = dep.upper()
+        destination = arr.upper()
+        # passenger_count = "1"
+        adult_count = adult
+        child_count = child
+        infant_count = infant
+        seat_type = seat[0]
+        travel_date = travel_date
 
-    driver.get(
-        f"https://tickets.paytm.com/flights/flightSearch/{source}/{destination}/{adult_count}/{child_count}/{infant_count}/{seat_type}/{travel_date}/")
-    driver.minimize_window()
-    driver.implicitly_wait(10)
+        driver.get(
+            f"https://tickets.paytm.com/flights/flightSearch/{source}/{destination}/{adult_count}/{child_count}/{infant_count}/{seat_type}/{travel_date}/")
+        driver.minimize_window()
+        driver.implicitly_wait(10)
 
-    flights_count = len(driver.find_elements(By.CLASS_NAME, "_2MkSl"))
-    total_flights = min(25, flights_count)
+        flights_count = len(driver.find_elements(By.CLASS_NAME, "_2MkSl"))
+        total_flights = min(25, flights_count)
 
-    prices = driver.find_elements(By.CLASS_NAME, "_2MkSl")[:total_flights]
-    carrier_name = driver.find_elements(By.CLASS_NAME, "_2cP56")[:total_flights]
-    dep_time = []
-    flight_duration = driver.find_elements(By.CLASS_NAME, "_1J4f_")[:total_flights]
-    arr_time = []
-    time = driver.find_elements(By.CLASS_NAME, "_3x8TR")[:(2 * total_flights)]
-    more_details = driver.find_elements(By.CLASS_NAME, "_3U68I")[:total_flights]
+        prices = driver.find_elements(By.CLASS_NAME, "_2MkSl")[:total_flights]
+        carrier_name = driver.find_elements(By.CLASS_NAME, "_2cP56")[:total_flights]
+        dep_time = []
+        flight_duration = driver.find_elements(By.CLASS_NAME, "_1J4f_")[:total_flights]
+        arr_time = []
+        time = driver.find_elements(By.CLASS_NAME, "_3x8TR")[:(2 * total_flights)]
+        more_details = driver.find_elements(By.CLASS_NAME, "_3U68I")[:total_flights]
 
-    flight_codes = []
-    for element in more_details:
-        element.click()
-        codes = driver.find_elements(By.CSS_SELECTOR, "._3tMEB span")
-        flight_codes.append(codes[1].text)
+        flight_codes = []
+        for element in more_details:
+            element.click()
+            codes = driver.find_elements(By.CSS_SELECTOR, "._3tMEB span")
+            flight_codes.append(codes[1].text)
 
-    print(f"Total flights found: {total_flights}")
+        print(f"Total flights found: {total_flights}")
 
-    for index in range(len(time)):
-        j = index + 1
-        if j % 2 == 1:
-            dep_time.append(time[index].text.replace('\n', ' '))
-        else:
-            arr_time.append(time[index].text.replace('\n', ' '))
+        for index in range(len(time)):
+            j = index + 1
+            if j % 2 == 1:
+                dep_time.append(time[index].text.replace('\n', ' '))
+            else:
+                arr_time.append(time[index].text.replace('\n', ' '))
 
-    dict_list = []
+        dict_list = []
 
-    for flight in range(len(prices)):
-        flight_info_dict = {'Airline': carrier_name[flight].text, 'Flight Code': flight_codes[flight],
-                            'Departure time': dep_time[flight], 'Arrival time': arr_time[flight],
-                            'Flight duration': flight_duration[flight].text,
-                            'Price': prices[flight].text}
-        dict_list.append(flight_info_dict)
-    # driver.close()
-    display(dict_list)
+        for flight in range(len(prices)):
+            flight_info_dict = {'Airline': carrier_name[flight].text, 'Flight Code': flight_codes[flight],
+                                'Departure time': dep_time[flight], 'Arrival time': arr_time[flight],
+                                'Flight duration': flight_duration[flight].text,
+                                'Price': prices[flight].text}
+            dict_list.append(flight_info_dict)
+        # driver.close()
+        display(dict_list)
+    except:
+        scrape_price_i(source, destination, travel_date,adult_count,child_count,infant_count,seat_type)
